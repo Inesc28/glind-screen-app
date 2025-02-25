7e6a660e48e07e3882af50c43744d3376b8926e8
Inesc28 <125275762+Inesc28@users.noreply.github.com>
2025-02-25 14:28:03 -0400

    server running

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado:', socket.id);
+
+  socket.on('requestScreenShare', () => {
+    socket.broadcast.emit('screenShareRequest', socket.id);
+  });
+
+  socket.on('acceptScreenShare', (fromSocketId) => {
+    socket.to(fromSocketId).emit('screenShareAccepted', socket.id);
+  });
+
+  socket.on('declineScreenShare', (fromSocketId) => {
+    socket.to(fromSocketId).emit('screenShareDeclined');
+  });
+
+  socket.on('screenData', (data) => {
+    socket.broadcast.emit('screenUpdate', data);
+  });
+
+  socket.on('locationUpdate', (data) => {
+    socket.broadcast.emit('locationUpdate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const SOCKET_URL = 'YOUR_SOCKET_SERVER_URL'; 
+const SOCKET_URL = 'http://192.168.1.20:3000'; 
 
 export const socket = io(SOCKET_URL, {
   transports: ['websocket'],
